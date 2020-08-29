2ee78eea8dbe73d00cad7639a96e2ec98490d688
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-08-29 17:09:25 +1000

    Add checking to see if as subscription to that team already exists

--- bot/commands/subscribe.js
+++ bot/commands/subscribe.js
@@ -6,12 +6,13 @@ const command = {
     aliases: [],
     description: "Subscribes a channel to a teams games",
     async execute(message, args) {
-        let err, docs = await subscriptions.find({channel_id: message.channel.id});
-        if(err) throw err;
-        if(docs.length > 0) return message.channel.send("You already have subscibed this channel to a team! use bb!unsubscibr to remove the subscription");
 
         let team = await getTeam(args.join(" "));
         if(!team) return message.channel.send("I can't find that team!");
+
+        let err, docs = await subscriptions.find({$or: [{channel_id: message.channel.id},{guild_id:message.guild.id, team:team.id}]});
+            if(err) throw err;
+            if(docs.length > 0) return message.channel.send("You already have subscibed this channel to a team, or this team to a channel! use bb!unsubscibr to remove the subscription");
 
         // eslint-disable-next-line no-unused-vars
         let savErr, doc = new subscriptions({
