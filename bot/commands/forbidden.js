b6af33cc5c0ca8ca4e4b0b19ee87545f2e741273
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-09-02 07:49:11 +1000

    Typo fix

--- bot/commands/forbidden.js
+++ bot/commands/forbidden.js
@@ -3,7 +3,7 @@ const {getGuild, saveGuild} = require("../util/guildUtils");
 const command = {
     name: "forbidden",
     aliases: [],
-    description: "Toggles if forbidden knowlage is disabled in this guild\nDefault: Hide forbidden knowlage\nbb!subscribe [team name]",
+    description: "Toggles if forbidden knowledge is disabled in this guild\nDefault: Hide forbidden knowlage\nbb!subscribe [team name]",
     async execute(message, args) {
 
         if(!message.guild) return message.channel.send("This command must be used in a guild!").catch(console.error);
