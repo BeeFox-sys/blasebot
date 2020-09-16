ec8154d8496fc5a3f64ba46b891070119a609ce6
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-09-16 12:08:21 +1000

    fix subscribe

--- bot/commands/subscribe-root.js
+++ bot/commands/subscribe-root.js
@@ -3,7 +3,7 @@ const { messageError } = require("../util/miscUtils");
 const command = {
     name: "subscribe",
     aliases: [],
-    description: "Subscribes to events from blaseball\nbb!subscibe",
+    description: "Subscribes to events from blaseball\nbb!subscribe",
     root: true,
     async execute(message, args) {
         message.channel.send("Use bb!help to see a list of events you can subscribe to").then(global.stats.messageFreq.mark()).catch(messageError);
