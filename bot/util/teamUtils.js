f1adbb006446eb92402af8536e8990f3a6463c96
Esther Bolik <estherbolik@gmail.com>
2021-04-13 06:49:10 +0100

    Update team cards for S12-13
    - Add Level, eDensity, Tarot Card, Times Evolved, Runs This Season, Wins
    - Add the site's names for the Bench and Bullpen
    - Link the embed title to the team page on the site
    - Replace Championships number with yellow dots
    - Rename Attributes -> Modifications to match site

--- bot/util/teamUtils.js
+++ bot/util/teamUtils.js
@@ -22,21 +22,30 @@ async function generateTeamCard(team, forbidden){
     let standings = games().standings;
     let wins = standings.wins[team.id] ?? 0;
     let losses = standings.losses[team.id] ?? 0;
+    let gamesPlayed = standings.gamesPlayed[team.id] ?? 0;
+    let runs = standings.runs[team.id] ?? 0;
+
     let teamCard = new MessageEmbed()
         .setTitle((Number(team.emoji)?String.fromCodePoint(team.emoji):team.emoji) + " " + team.fullName)
         .setColor(team.mainColor)
-        .addField("Lineup",team.lineup.length?playerList(team.lineup):"uhhhhh...",true)
-        .addField("Rotation",team.rotation.length?playerList(team.rotation):"uhhhhh...",true);
-    if(forbidden) teamCard.addField("Bullpen", team.bullpen.length?"||"+playerList(team.bullpen)+"||":"uhhhhh...",true)
-        .addField("Bench", team.bench.length?"||"+playerList(team.bench)+"||":"uhhhhh...", true);
-    teamCard.addField("Championships",team.championships, true)
-        .addField("Attributes", await attributes(team)||"None",true)
+        .setURL("https://www.blaseball.com/team/" + team.id)
+        .addField("Lineup",team.lineup.length?playerList(team.lineup):"*Empty*",true)
+        .addField("Rotation",team.rotation.length?playerList(team.rotation):"*Empty*",true);
+    if(forbidden) teamCard.addField("Shadow Lineup (Bullpen)", team.bullpen.length ? "||" + playerList(team.bullpen) + "||" : "||*Empty*||", true)
+        .addField("Shadow Rotation (Bench)", team.bench.length ? "||" + playerList(team.bench) + "||" : "||*Empty*||", true);
+    teamCard.addField("Modifications", await attributes(team)||"None",true)
+        .addField("Level", creditLevels[team.level] || "-", true)
+        .addField("eDensity", team.eDensity.toFixed(5) + " bl/m³", true)
+        .addField("Tarot Card", tarotCards[team.card] || "---- -----", true)
+        .addField("Championships","🟡".repeat(team.championships) || "** **", true)
+        .addField("Times Evolved", team.evolution, true)
         .addField("Been Shamed",team.totalShames, true)
         .addField("Shamed Others",team.totalShamings,true)
+        .addField("Runs This Season", runs, true)
         .addField("Been Shamed This Season", team.seasonShames, true)
         .addField("Shamed Others This Season", team.seasonShamings, true)
-        .addField("Win-Loss",`${wins}-${losses}`)
-        .setFooter(`${team.slogan} | ID: ${team.id}`);
+        .addField("Season Record",`${wins} Wins (${gamesPlayed - losses}-${losses})`)
+        .setFooter(`${team.slogan} | ${team.shorthand} | ID: ${team.id}`);
     return teamCard;
 }
 
@@ -84,9 +93,48 @@ const nullTeam = {
     slogan: "I AM ERROR"
 };
 
+const tarotCards = {
+    '-1': "Fool",
+    0: "I The Magician",
+    1: "II The High Priestess",
+    2: "III The Empress",
+    3: "IIII The Emperor",
+    4: "V The Hierophant",
+    5: "VI The Lover",
+    6: "VII The Chariot",
+    7: "VIII Justice",
+    8: "VIIII The Hermit",
+    9: "X The Wheel of Fortune",
+    10: "XI Strength",
+    11: "XII The Hanged Man",
+    12: "XIII",
+    13: "XIIII Temperance",
+    14: "XV The Devil",
+    15: "XVI The Tower",
+    16: "XVII The Star",
+    17: "XVIII The Moon",
+    18: "XVIIII The Sun",
+    19: "XX Judgment"
+}
+
+const creditLevels = {
+    0: "0D",
+    1: "1D",
+    2: "2D",
+    3: "3D",
+    4: "C",
+    5: "Low A",
+    6: "High A",
+    7: "AA",
+    8: "AAA",
+    9: "AAAA",
+    10: "AAAAA"
+}
 
 module.exports = {
     getTeam: getTeam,
     generateTeamCard: generateTeamCard,
-    nullTeam:nullTeam
+    nullTeam: nullTeam,
+    tarotCards: tarotCards,
+    creditLevels: creditLevels
 };
